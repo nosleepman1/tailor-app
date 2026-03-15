1386b165efe5748b8e8460f0d5b3cb3ff5242ea8
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-03-15 17:38:00 +0000

    refactor user and client tables: rename fields for consistency and add new attributes

--- server/database/migrations/0001_01_01_000000_create_users_table.php
+++ server/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'client'])->default('client');
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
