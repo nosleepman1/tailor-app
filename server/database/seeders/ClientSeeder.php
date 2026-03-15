fa58ce664ac72b9a13bb78a36c9eac62b60f9769
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-03-15 20:05:29 +0000

    add API routes for user and client resources; update Client and User factories; modify DatabaseSeeder for bulk creation

--- server/database/seeders/ClientSeeder.php
+++ server/database/seeders/ClientSeeder.php
@@ -12,6 +12,6 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        
     }
 }
---
+++ server/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\api\v1\ClientController;
+use App\Http\Controllers\api\v1\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::apiResource("v1/users", UserController::class);
+Route::apiResource("v1/clients", ClientController::class)->middleware("auth:sanctum");
