b01ee961584a731882cf1c94dc25a0d66d3f2429
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-03-22 03:49:22 +0000

    feat: server side bugs fix and add logout and me endpoints

--- server/app/Models/Client.php
+++ server/app/Models/Client.php
@@ -2,11 +2,27 @@
 
 namespace App\Models;
 
+use Database\Factories\ClientFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    /** @use HasFactory<\Database\Factories\ClientFactory> */
+    /** @use HasFactory<ClientFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'firstname', 'lastname', 'phone', 'price', 'is_paid', 'livre', 'manche',
+        'epaule', 'poitrine', 'taille', 'hanche', 'cou',
+        'pantalon', 'fesse', 'cuisse', 'biceps', 'bras',
+        'model_image', 'tissus_image',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_paid' => 'boolean',
+            'livre' => 'boolean',
+        ];
+    }
 }
